e280014d8e5ae60150818ed8bbfde8a680a7c9e8
CaioQC <caioq2004@gmail.com>
2025-04-07 21:14:46 -0300

    Updating users listing

---
+++ src/user/dto/ListUsers.dto.ts
@@ -0,0 +1,5 @@
+export class ListUsersDTO{
+    constructor(readonly name : string, readonly id : string){
+
+    }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Post, Get } from "@nestjs/common";
 import { UserRepository } from "./user.repository";
 import { CreateUserDTO } from "./dto/CreateUser.dto";
+import { UserEntity } from "./user.entity";
+import { v4 as uuid } from "uuid";
+import { ListUsersDTO } from "./dto/ListUsers.dto";
 
 @Controller("/users")
 export class UserController{
@@ -11,12 +14,25 @@ export class UserController{
 
     @Post()
     async createUser(@Body() usersData: CreateUserDTO){
-        this.userRepository.save(usersData)
-        return usersData
+        const userEntity = new UserEntity()
+
+        userEntity.id = uuid()
+        userEntity.name = usersData.name
+        userEntity.email = usersData.email
+        userEntity.password = usersData.password
+        
+        this.userRepository.save(userEntity)
+        return { 
+            user : new ListUsersDTO(userEntity.name, userEntity.id),
+            message : "User successfully created!"
+        }
     }
 
     @Get()
     async showUsers(){
-        return this.userRepository.show()
+        const savedUsers = await this.userRepository.show()
+        const usersList = savedUsers.map(user => new ListUsersDTO(user.name, user.id))
+
+        return usersList
     }
 }
